ab7007a4e0ee9abe11e1c8c5fca5c56a6dfde65a
Carlson Santana Cruz <carlsonsantana@gmail.com>
2025-12-24 08:56:35 -0300

    Allow to sign APK

--- run.sh
+++ run.sh
@@ -2,7 +2,19 @@
 
 set -e
 
-rm -f /tmp/mugen-unsigned.apk /output/mugen-aligned.apk
+rm -f /tmp/mugen-unsigned.apk /tmp/mugen-aligned.apk /output/mugen-aligned.apk /output/mugen-signed.apk
+
+if [ -f "/game_certificate.key" ]; then
+  if [ -z "$GAME_KEYSTORE_PASSWORD" ] || [ -z "$GAME_KEYSTORE_KEY_ALIAS" ] || [ -z "$GAME_KEYSTORE_KEY_PASSWORD" ]; then
+    echo "ERROR: Partial keystore configuration detected."
+    echo "You must provide ALL THREE variables, when pass '/game_certificate.key' VOLUME."
+    echo "Missing values for: "
+    [ -z "$GAME_KEYSTORE_PASSWORD" ] && echo "- GAME_KEYSTORE_PASSWORD"
+    [ -z "$GAME_KEYSTORE_KEY_ALIAS" ] && echo "- GAME_KEYSTORE_KEY_ALIAS"
+    [ -z "$GAME_KEYSTORE_KEY_PASSWORD" ] && echo "- GAME_KEYSTORE_KEY_PASSWORD"
+    exit 1
+  fi
+fi
 
 # Convert icons
 magick /icon.png -resize 72x72 /mugen-android/res/drawable-hdpi/icon.png
@@ -20,6 +32,13 @@ cp -r /mugen /mugen-android/assets
 
 # Build an aligned version of the Android app
 java -jar /apktool/apktool.jar b /mugen-android -o /tmp/mugen-unsigned.apk
-zipalign -v -p 4 /tmp/mugen-unsigned.apk /output/mugen-aligned.apk
+zipalign -v -p 4 /tmp/mugen-unsigned.apk /tmp/mugen-aligned.apk
+
+if [ -f "/game_certificate.key" ]; then
+  java -jar /opt/signmyapp.jar -ks /game_certificate.key -ks-pass "$GAME_KEYSTORE_PASSWORD" -ks-key-alias "$GAME_KEYSTORE_KEY_ALIAS" -key-pass "$GAME_KEYSTORE_KEY_PASSWORD" -in /tmp/mugen-aligned.apk -out /output/mugen-signed.apk
+  rm /tmp/mugen-aligned.apk
+else
+  mv /tmp/mugen-aligned.apk /output/mugen-aligned.apk
+fi
 
 rm /tmp/mugen-unsigned.apk
